22d95589e67e567b0e4b4edf0a1a4720a6a99bf8
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-11-15 19:49:01 -0300

    feat: serialize and deserialize functions

--- mod.ts
+++ mod.ts
@@ -1,4 +1,62 @@
+import type {
+  DeserializeOptions,
+  SerializeOptions,
+} from "./src/interfaces/mod.ts";
+import { Database } from "./src/classes/mod.ts";
+import { unknownDeserializer } from "./src/deserializers/mod.ts";
+import { unknownSerializer } from "./src/serializers/mod.ts";
+
 export * from "./src/deserializers/mod.ts";
 export * from "./src/types/mod.ts";
 export * from "./src/serializers/mod.ts";
 export * from "./src/classes/mod.ts";
+
+export interface ISerializeDeserializeResponse<T> {
+  value: T;
+  objectDatabase: Database<object>;
+  stringDatabase: Database<string>;
+}
+
+export function serialize(
+  value: unknown,
+  options?: Partial<SerializeOptions>,
+): ISerializeDeserializeResponse<Uint8Array> {
+  const objectDatabase = options?.objectDatabase ?? new Database<object>();
+  const stringDatabase = options?.stringDatabase ?? new Database<string>();
+  const buffer = unknownSerializer(value, {
+    objectDatabase,
+    stringDatabase,
+    plainText: options?.plainText ?? false,
+    serializers: options?.serializers ?? [],
+  });
+  
+  const result = {
+    value: buffer,
+    objectDatabase,
+    stringDatabase,
+  } satisfies ISerializeDeserializeResponse<Uint8Array>;
+
+  return result;
+}
+
+export function deserialize<T = unknown>(
+  buffer: Uint8Array,
+  options?: Partial<DeserializeOptions>,
+): ISerializeDeserializeResponse<T> {
+  const objectDatabase = options?.objectDatabase ?? new Database<object>();
+  const stringDatabase = options?.stringDatabase ?? new Database<string>();
+  const value = unknownDeserializer(buffer, {
+    offset: options?.offset ?? 0,
+    objectDatabase,
+    stringDatabase,
+    deserializers: options?.deserializers ?? [],
+  }) as T;
+
+  const result = {
+    value,
+    objectDatabase,
+    stringDatabase,
+  } satisfies ISerializeDeserializeResponse<T>;
+
+  return result;
+}
