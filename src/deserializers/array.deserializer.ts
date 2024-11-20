b10490bd105e875b8442d05c4d3d2f7a9ebd4454
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-11-20 16:25:59 -0300

    fix: empty arrays

--- src/deserializers/array.deserializer.ts
+++ src/deserializers/array.deserializer.ts
@@ -2,12 +2,13 @@ import { Opcode } from "../enums/mod.ts";
 import type { DeserializeOptions } from "../interfaces/mod.ts";
 import { unknownDeserializer } from "./unknown.deserializer.ts";
 
+// TODO: Improve this...
 export function arrayDeserializer(
   serialized: Uint8Array,
   options: DeserializeOptions,
 ): unknown[] {
   const result: unknown[] = [];
-
+  const currentOffset = options.offset;
   options.offset++;
   options.objectDatabase.getOrInsert(result);
 
@@ -16,7 +17,9 @@ export function arrayDeserializer(
     const opcode = serialized.at(options.offset)!;
 
     if (opcode === undefined || opcode === Opcode.EndArray) {
-      if (opcode === Opcode.EndArray) {
+      if (
+        opcode === Opcode.EndArray && (options.offset - 1) !== currentOffset
+      ) {
         result.push(value);
       }
       break;
