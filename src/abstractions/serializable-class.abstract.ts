30bdc573498fae2fcd5f5102998464ca822b7301
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-12-15 22:38:37 -0300

    better serialize method

--- src/abstractions/serializable-class.abstract.ts
+++ src/abstractions/serializable-class.abstract.ts
@@ -3,6 +3,5 @@ import type { SerializedClass } from "../interfaces/mod.ts";
 import type { RequireAtLeastOne } from "../types/mod.ts";
 
 export abstract class SerializableClass {
-  public serialize?(): RequireAtLeastOne<SerializedClass<any>>;
   public static deserialize?(serialized: Partial<SerializedClass<any>>): any;
 }
