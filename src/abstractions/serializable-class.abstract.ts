c6f43f4b6ae58027f265aefda1dd6ad2ef7f6ba9
Iuriiiiiii <alex.casas@webtroniclabs.com>
2025-01-09 19:27:40 -0300

    more docs

--- src/abstractions/serializable-class.abstract.ts
+++ src/abstractions/serializable-class.abstract.ts
@@ -1,6 +1,14 @@
 // deno-lint-ignore-file
 import type { SerializedClass } from "../interfaces/mod.ts";
 
+/**
+ * Represents a class that can be serialized.
+ */
 export abstract class SerializableClass {
+  /**
+   * 
+   * @param serialized - The serialized class data
+   * @returns The current class.
+   */
   public static deserialize?(serialized: Partial<SerializedClass<any>>): any;
 }
